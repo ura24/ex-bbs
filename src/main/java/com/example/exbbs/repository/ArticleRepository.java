89b89fd66ed600d1050bea1c0a207b5adc673280
Yuto Urashima <yuto.urashima24@gmail.com>
2023-08-02 15:19:22 +0900

    Modify ArticleRepository.java

--- src/main/java/com/example/exbbs/repository/ArticleRepository.java
+++ src/main/java/com/example/exbbs/repository/ArticleRepository.java
@@ -30,7 +30,7 @@ public class ArticleRepository {
      * @return 記事全件
      */
     public List<Article> findAll() {
-        String sql = "SELECT id, name, content FROM articles";
+        String sql = "SELECT id, name, content FROM articles ORDER BY id DESC";
         List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
         return articleList;
     }
